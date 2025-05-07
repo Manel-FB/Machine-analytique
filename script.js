e6e0683bc0d9cd102f6577194e3f8111c3afea8f
Manel FB <157753352+Manel-FB@users.noreply.github.com>
2025-05-07 22:16:10 +0200

    Added custom cursor

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-const sections = document.getElementsByTagName("section");
-console.log(sections);
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        console.log(`Section visible :`, entry.target);
-      }
-    });
-  },
-  { threshold: 0.5 } // Détecte quand au moins 50% de la section est visible
-);
-
-sections.forEach((section) => observer.observe(section));
-
-for (let i = 0; i < sections.length; i++) {
-  console.log(sections[i]);
-  sections[i].addEventListener("scroll", (event) => {
-    // scrollToSection(sections[i+1]);
-    console.log(event);
-  });
+const cursorBox = document.getElementById("cursor-box");
+function updateCursor(e) {
+  cursorBox.style.left = `calc(${e.clientX}px)`;
+  cursorBox.style.top = `calc(${e.clientY}px)`;
 }
+
+document.addEventListener("mousemove", (e) => {
+  updateCursor(e);
+});
