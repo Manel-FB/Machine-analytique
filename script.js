7560c050f2cdc64f2b388cec59f314141b22961e
Manel FB <manel.frit.barboule@gmail.com>
2025-05-07 21:32:51 +0200

    Update

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const sections = document.getElementsByTagName('section');
+const sections = document.getElementsByTagName("section");
+console.log(sections);
 
-for (let i; i < sections.length; i++) {
-    sections[i].addEventListener('scroll', (event) => {
-        scrollToSection(sections[i+1]);
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(`Section visible :`, entry.target);
+      }
     });
+  },
+  { threshold: 0.5 } // Détecte quand au moins 50% de la section est visible
+);
+
+sections.forEach((section) => observer.observe(section));
+
+for (let i = 0; i < sections.length; i++) {
+  console.log(sections[i]);
+  sections[i].addEventListener("scroll", (event) => {
+    // scrollToSection(sections[i+1]);
+    console.log(event);
+  });
 }
