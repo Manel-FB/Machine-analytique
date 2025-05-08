318501ec6d4aa9538356b3ad2b1206fafd372df2
Manel FB <157753352+Manel-FB@users.noreply.github.com>
2025-05-08 17:26:47 +0200

    Charles Babbage added

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function updateCursor(e) {
 document.addEventListener("mousemove", (e) => {
   updateCursor(e);
 });
+
+const links = document.querySelectorAll("a");
+links.forEach((link) => {
+  link.addEventListener("mouseenter", (e) => {
+    cursorBox.style.transform = "translate(-50%, -50%) scale(150%)";
+  });
+
+  link.addEventListener("mouseleave", (e) => {
+    cursorBox.style.transform = "translate(-50%, -50%) scale(100%)";
+  });
+});
